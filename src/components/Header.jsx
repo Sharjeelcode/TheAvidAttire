4fdbd348f48d15634cfcacc4b993510aeae46031
unknown <sharjeelahmed3808@gmail.com>
2024-05-18 03:51:35 +0500

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import logo from "../assets/logoicon.png";
+import logo from "../assets/logo.png";
 import TopHeader from "./TopHeader";
 import { FaHeart } from "react-icons/fa6";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaAlignJustify } from "react-icons/fa6";
+import HeroImg from "./HeroImg";
 
 function Header() {
   const [mobileHeader, setMobileHeader] = useState(false);
@@ -12,28 +13,18 @@ function Header() {
   };
   return (
     <>
-      <img
-        src="https://outfitbydk.com/cdn/shop/files/BN_2000x.jpg?v=1713370985"
-        alt=""
-        className="fixed top-0 h-full w-full z-[-1] hidden lg:block"
-      />
-      <img
-        src="https://outfitbydk.com/cdn/shop/files/44245_700x.jpg?v=1713364554"
-        alt=""
-        className="fixed top-0 h-full w-full z-[-1] block lg:hidden"
-      />
       <TopHeader />
       {/* //mobile header */}
       {mobileHeader && (
         <span
           onClick={handleMobileHeader}
-          className="absolute top-0 right-0 bg-gray-200 w-[20vw] h-[11vh] flex justify-center items-center"
+          className="fixed top-20 left-[80vw] bg-gray-200 w-[15vw] h-[10vh] flex justify-center items-center lg:hidden"
         >
           x
         </span>
       )}
       {mobileHeader && (
-        <div className=" lg:hidden flex flex-col gap-4 pt-10 pl-4 w-[80vw] h-full absolute top-0 bg-white ">
+        <div className=" lg:hidden flex flex-col gap-4 pt-10 pl-4 w-[80vw] h-[100vh]  bg-white fixed top-0  z-[100]">
           <h1 className="cursor-pointer">HOME</h1>
           <hr />
           <h1 className="cursor-pointer">SHIRTS</h1>
@@ -49,34 +40,32 @@ function Header() {
           <h1 className="cursor-pointer">BEST SELLERS</h1>
         </div>
       )}
-      <div className="flex h-16 items-center justify-between px-4 lg:px-8 pt-1">
-        <FaAlignJustify
-          onClick={handleMobileHeader}
-          className="lg:hidden text-white"
-        />
+      <div className="sticky top-0 bg-white flex py-1 items-center justify-between px-4 lg:px-8 pt-1 ">
+        <FaAlignJustify onClick={handleMobileHeader} className="lg:hidden " />
         <div className="flex items-center  gap-2 cursor-pointer">
           <img src={logo} alt="" className="w-16   lg:w-20 lg:h-16 " />
           <div>
-            <h1 className=" font-bold  text-white">The Avid Attire</h1>
-            <p className="text-xs text-center font-semibold text-white">
+            <h1 className=" font-bold  ">The Avid Attire</h1>
+            <p className="text-xs text-center font-semibold ">
               Wear you passion
             </p>
           </div>
         </div>
-        <div className="hidden lg:flex gap-4 text-white">
-          <h1 className="cursor-pointer">HOME</h1>
-          <h1 className="cursor-pointer">SHIRTS</h1>
-          <h1 className="cursor-pointer">OVERSIZED T-SHIRTS</h1>
-          <h1 className="cursor-pointer">PLAIN T-SHIRTS</h1>
-          <h1 className="cursor-pointer">RAGULAR FIT T-SHIRTS</h1>
-          <h1 className="cursor-pointer">BOTTOM WEAR</h1>
-          <h1 className="cursor-pointer">BEST SELLERS</h1>
+        <div className="hidden lg:flex gap-4 ">
+          <h1 className="cursor-pointer font-semibold">HOME</h1>
+          <h1 className="cursor-pointer font-semibold">SHIRTS</h1>
+          <h1 className="cursor-pointer font-semibold">OVERSIZED T-SHIRTS</h1>
+          <h1 className="cursor-pointer font-semibold">PLAIN T-SHIRTS</h1>
+          <h1 className="cursor-pointer font-semibold">RAGULAR FIT T-SHIRTS</h1>
+          <h1 className="cursor-pointer font-semibold">BOTTOM WEAR</h1>
+          <h1 className="cursor-pointer font-semibold">BEST SELLERS</h1>
         </div>
         <div className="flex gap-4">
-          <FaHeart className="cursor-pointer text-white" />
-          <FaCartShopping className="cursor-pointer text-white" />
+          <FaHeart className="cursor-pointer " />
+          <FaCartShopping className="cursor-pointer " />
         </div>
       </div>
+      <HeroImg />
     </>
   );
 }
---
+++ src/components/HeroImg.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroImg() {
+  return (
+    <>
+      <img
+        src="https://thehangerpakistan.com/cdn/shop/files/db437a06-f20e-40c4-9038-6034cbb372ff_1512x851.jpg?v=1711411569"
+        alt=""
+        className=" h-full w-full "
+      />
+    </>
+  );
+}
+
+export default HeroImg;
